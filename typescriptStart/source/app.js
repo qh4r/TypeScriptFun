3bdf96888a6459b9953898922e69f5f7da5009f7
Rafa³ Kucharski <r.kucharski@lgbs.pl>
2016-11-01 19:44:20 +0100

    Start of typescript lookthrough

---
+++ typescriptStart/source/app.js
@@ -0,0 +1,5 @@
+console.log('test');
+var $elem = document.createElement("h1");
+$elem.innerText = "DZIALA";
+document.querySelector('body').appendChild($elem);
+//# sourceMappingURL=app.js.map
---
+++ typescriptStart/source/app.ts
@@ -0,0 +1,5 @@
+console.log('test');
+
+const $elem = document.createElement("h1");
+$elem.innerText = "DZIALA";
+document.querySelector('body').appendChild($elem);
