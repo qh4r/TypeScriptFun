ec55db8f4800287f2daf202f9550f4462e5d8206
Rafał Kucharski <r.kucharski@lgbs.pl>
2016-11-02 22:51:39 +0100

    Custom types tests

--- typescriptStart/source/objects.js
+++ typescriptStart/source/objects.js
@@ -4,11 +4,20 @@ var object1 = {
 };
 //NASTEPUJE INFERENCJA TYPU
 //od teraz utworzony obiekt musi miec pola name i age o odpowiednich typach i nic poza tym
-// object1 = {} // error wymagane pola name i age jako string i age
+//object1 = {}; // error wymagane pola name i age jako string i age
 // object1 = {name: "tomasz", boring: true} // j/w brakuje age
 // object1 = {name: "tomasz", age: 32, boring: true}; // nadmiar tez odpada
 var object2 = {
     length: 31,
     name: "Długa"
+};
+var house;
+//house = {
+//    test: "Sobieskiego"
+//}; nie zadziala wymagany typ Address
+//noinspection TsLint
+house = {
+    street: 'Kazimierza',
+    apartment: 5
 };
 //# sourceMappingURL=objects.js.map
--- typescriptStart/source/objects.ts
+++ typescriptStart/source/objects.ts
@@ -6,11 +6,26 @@ let object1 = {
 //NASTEPUJE INFERENCJA TYPU
 //od teraz utworzony obiekt musi miec pola name i age o odpowiednich typach i nic poza tym
 
-// object1 = {} // error wymagane pola name i age jako string i age
+ //object1 = {}; // error wymagane pola name i age jako string i age
 // object1 = {name: "tomasz", boring: true} // j/w brakuje age
 // object1 = {name: "tomasz", age: 32, boring: true}; // nadmiar tez odpada
 
 let object2 : {name: string, length: number} = { // jawna dekalracja typow
     length: 31,
     name: "Długa"
+};
+
+// typ to nie klasa tylko wymagany format
+// ? okresla nullable
+type Address = {street: string, block?: string, apartment: number};
+
+let house : Address;
+//house = {
+//    test: "Sobieskiego"
+//}; nie zadziala wymagany typ Address
+
+//noinspection TsLint
+house = {
+    street: 'Kazimierza',
+    apartment: 5
 };
